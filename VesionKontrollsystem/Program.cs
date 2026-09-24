432f05f54cd13a3fbae6212c876cbdff215c487e
fabriceabouem-hub <fabriceabouem585@gmail.com>
2026-09-24 03:01:40 +0200

    Implement Person class and comparison in Main method
    Added Person class with comparison logic and updated Main method to create and compare Person instances.

--- VesionKontrollsystem/Program.cs
+++ VesionKontrollsystem/Program.cs
@@ -4,8 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World! ");
-
+            Person person1 = new Person("Fab", "Dyl", 15);
+            Person person2 = new Person("Siut", "WIOU", 51);
+            int result = person1.CompareTo(person2);
+            if (result<0)
+            {
+                Console.WriteLine($"{persone1.Name} ist junger alt {person2.Name} ");
+            }
+            if (result==0)
+            {
+                Console.WriteLine($"{persone1.Name} ist gleicher alt {person2.Name} ");
+            }
+            if (result>0)
+            {
+                Console.WriteLine($"{persone1.Name} ist junger alt {person2.Name} ");
+            }
         }
     }
+        class Person
+        {
+            public string Name {get; set;}
+            public string Vorname {get; set;}
+            public int Alt {get; set;}
+            public Person(string name, string vorname, int alt)
+            {
+                Name = name;
+                Vorname = vorname;
+                Alt = alt;
+            }
+            public int CompareTo(Person other)
+            {
+                if(other == null) return 1;
+               {
+                  return Alt.CompareTo (other Alt) ;
+               }
+            }
+        }
 }
